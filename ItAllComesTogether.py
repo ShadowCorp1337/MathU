ca36316a51e1d0c82cbd4a8acaa046640346b174
Franco-Jacobs <100114283+Franco-Jacobs@users.noreply.github.com>
2022-04-27 12:10:48 +0200

    creates mathML array

--- ItAllComesTogether.py
+++ ItAllComesTogether.py
@@ -2,6 +2,7 @@ import sys
 import os
 from GetAllProbFromFile import *
 from FindAllJson import *
+from StringToMML import *
 
 Path = input()
 if not(bool(Path)):
@@ -22,10 +23,13 @@ for x in AllProbs:
     y = x.problem.en.latex
     #print(y)
     if str(y).find('\(')==0:
-        nonWordStarters.append(y)
-        #print(x.problem.en.latex)
-    #else:
-        #print('unlucky')
+        z = str(y).replace('\(','')
+        z = str(z).replace('\)','')
+        if len(z) < 51:
+            nonWordStarters.append(z)
+#for a in nonWordStarters:
+    #print(a)
 
-for a in nonWordStarters:
-    print(a)
+arrMathML = arrToMML(nonWordStarters)
+for b in arrMathML:
+    print(b+'\n\n')
--- StringToMML.py
+++ StringToMML.py
@@ -1,4 +1,10 @@
 import latex2mathml.converter
 latex_input = "3x^{2} + 19x + 6"
-mathml_output = latex2mathml.converter.convert(latex_input)
-print(mathml_output)
+
+def arrToMML(arrStrings):
+    arrMathml = []
+    for i in arrStrings:
+      arrMathml.append(latex2mathml.converter.convert(i))
+    return arrMathml
+#mathml_output = latex2mathml.converter.convert(latex_input)
+#print(mathml_output)
